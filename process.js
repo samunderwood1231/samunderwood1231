cedbf4bf9e3bf2a37f9624c04f267275604fc623
samunderwood1231 <samunderwood1231@gmail.com>
2023-09-04 22:04:34 -0700

    Add files via upload

---
+++ process.js
@@ -0,0 +1,24 @@
+document.getElementById('process').onclick = function () { process(); };
+
+function process() {
+       let int1 = parseInt(document.forms["numInput"].elements["int1"].value);
+    let int2 = parseInt(document.forms["numInput"].elements["int2"].value);
+    let int3 = parseInt(document.forms["numInput"].elements["int3"].value);
+    if (isNaN(int1) || isNaN(int2) || isNaN(int2)) {
+        alert("Please enter three integers!")
+        console.log("one or more inputs was NaN")
+    } else {
+        console.log("Processing...")
+        let sum = int1 + int2 + int3;
+        let product = int1 * int2 * int3;
+        let average = parseFloat(sum / 3);
+        let min = Math.min(int1, int2, int3);
+        let max = Math.max(int1, int2, int3);
+        document.forms["numInput"].elements["sum"].value = sum;
+        document.forms["numInput"].elements["product"].value = product;
+        document.forms["numInput"].elements["average"].value = average;
+        document.forms["numInput"].elements["min"].value = min;
+        document.forms["numInput"].elements["max"].value = max;
+        
+    }
+}
