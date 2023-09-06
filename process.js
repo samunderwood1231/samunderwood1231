e2f8e53176d30b646083fa542a561f72ba100e11
samunderwood1231 <samunderwood1231@gmail.com>
2023-09-05 20:12:49 -0700

    Add files via upload

--- process.js
+++ process.js
@@ -5,7 +5,11 @@ function process() {
     let int2 = parseInt(document.forms["numInput"].elements["int2"].value);
     let int3 = parseInt(document.forms["numInput"].elements["int3"].value);
     if (isNaN(int1) || isNaN(int2) || isNaN(int2)) {
-        alert("Please enter three integers!")
+        document.forms["numInput"].elements["sum"].value = "Please enter an integer!";
+        document.forms["numInput"].elements["product"].value = "Please enter an integer!";
+        document.forms["numInput"].elements["average"].value = "Please enter an integer!";
+        document.forms["numInput"].elements["min"].value = "Please enter an integer!";
+        document.forms["numInput"].elements["max"].value = "Please enter an integer!";
         console.log("one or more inputs was NaN")
     } else {
         console.log("Processing...")
@@ -14,11 +18,6 @@ function process() {
         let average = parseFloat(sum / 3);
         let min = Math.min(int1, int2, int3);
         let max = Math.max(int1, int2, int3);
-        document.forms["numInput"].elements["sum"].value = sum;
-        document.forms["numInput"].elements["product"].value = product;
-        document.forms["numInput"].elements["average"].value = average;
-        document.forms["numInput"].elements["min"].value = min;
-        document.forms["numInput"].elements["max"].value = max;
-        
+        document.forms["numInput"].elements["numOutput"].value = `Sum: ${sum}\nProduct: ${product}\nAverage: ${average}\nMin value: ${min}\nMax value: ${max}`;
     }
 }
