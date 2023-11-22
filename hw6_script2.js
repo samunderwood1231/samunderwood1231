b667f7847f6f3da966a0eaddd98afe595ce86489
samunderwood1231 <samunderwood1231@gmail.com>
2023-11-21 20:44:59 -0800

    Add files via upload

---
+++ hw6_script2.js
@@ -0,0 +1,63 @@
+// need to add:
+// popup window
+// to lowerCase method
+
+//hw6_script1.js
+// goes with hw6_part1.html
+
+// add event listener on the window
+
+window.addEventListener("load", onLoad);
+
+// register event listener on submit button & bind submits button to getValues()
+function onLoad() {
+    document.forms['inputForm'].addEventListener('submit', function (event) {
+        event.preventDefault();
+        getValues();
+    });
+
+    // sets reset button to also hide div element showing successful submission
+    let resetBtn = document.getElementById("reset-button");
+    resetBtn.addEventListener("click", function () {
+        document.getElementById("inputForm").reset();
+        const div = document.getElementById("div");
+        div.textContent = "";
+        
+    });
+}
+
+// call validation methods
+function getValues() {
+    const div = document.getElementById("div");
+    let input = document.getElementById('input').value;
+    let search = document.getElementById('searchKey').value;
+    
+    if (input === "") {
+        let newWindow = window.open("", "error", "scrollbars=yes,resizable=yes,top=200,left=500,width=200,height=100");
+        newWindow.document.write(`Please enter some text to search!<p>`);
+        return;
+    }
+    if (search === "") {
+        newWindow = window.open("", "error", "scrollbars=yes,resizable=yes,top=200,left=500,width=200,height=100");
+        newWindow.document.write(`Please enter a search key!<p>`);
+        return;
+    }
+        div.textContent = '';
+        let charNum = countChar(input, search);
+        div.textContent = `The character "${search}" appeared ${charNum} times`;
+}
+    
+
+function countChar(input, searchKey) {
+    let charCounter = 0;
+    for (let i = 0; i < input.length; i++) {
+        const thisChar = input[i];
+        if (searchKey === thisChar) {
+           charCounter++;
+        }
+    }
+    return charCounter;
+}
+
+
+
---
+++ hw6_script3.js
@@ -0,0 +1,50 @@
+//hw6_script3.js
+// goes with hw6_part3.html
+
+// add event listener on the window
+window.addEventListener("load", onLoad);
+
+// register event listener on submit button & bind submits button to getValues()
+function onLoad() {
+    document.forms['inputForm'].addEventListener('submit', function (event) {
+        event.preventDefault();
+        validate();
+    });
+
+    // sets reset button to also hide div element showing successful submission
+    let resetBtn = document.getElementById("reset-button");
+    resetBtn.addEventListener("click", function () {
+        document.getElementById("inputForm").reset();
+        const div = document.getElementById('div-error');
+        div.textContent = "";
+    });
+
+}
+
+// call validation methods
+function validate() {
+    const div = document.getElementById('div-error');
+    let input = document.getElementById('phoneNum').value;
+
+    if (input === "") {
+        div.textContent = 'Please enter a phone number';
+    } else {
+        var reg = /^\(?(\d{3})\)? (\d{3})[-](\d{4})$/;
+
+        if (reg.test(input)) {
+            let input = document.getElementById('phoneNum').value;
+            const areaCode = input.slice(1, 4);
+            const firstThree = input.slice(6, 9);
+            const lastFour = input.slice(10, 15);
+            div.textContent = '';
+            document.forms['inputForm'].elements['areaCode'].value = areaCode;
+            document.forms['inputForm'].elements['firstThree'].value = firstThree;
+            document.forms['inputForm'].elements['lastFour'].value = lastFour;
+        } else {
+            div.textContent = 'Please enter a phone number in the format: (999) 999-9999';
+            document.forms['inputForm'].elements['areaCode'].value = '';
+            document.forms['inputForm'].elements['firstThree'].value = '';
+            document.forms['inputForm'].elements['lastFour'].value = '';
+        }
+    }
+}
